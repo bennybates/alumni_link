6428ee405a8be02ab8ad4e15dbc955896eb0a10b
Ben Bates <bates.benjamin@gmail.com>
2012-04-12 19:10:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlumniConnect::Application.config.secret_token = '4e8e745d2823bddc3d2332d8848feed1a5cfb771e3cf369bd34595cc831de36e7639cf962db00245bc3653393cc8910e628e1b0e3863fddd4b2894a1734a366f'
